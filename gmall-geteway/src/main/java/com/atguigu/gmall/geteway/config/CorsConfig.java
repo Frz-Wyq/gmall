47bac13e351e9a7eb0743061e1c24e861fb61737
frz <2694290124@qq.com>
2021-06-23 21:15:23 +0800

    项目基本搭建完成

---
+++ gmall-geteway/src/main/java/com/atguigu/gmall/geteway/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.atguigu.gmall.geteway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        //允许跨域的域名。*代表所有的域名,无法携带cookie
+        config.addAllowedOrigin("http://manager.gmall.com");
+        config.addAllowedOrigin("http://localhost:1000");
+        config.addAllowedOrigin("http://192.168.137.1:1000");
+        //允许所有的请求方式跨域访问：get put delete post
+        config.addAllowedMethod("*");
+        //允许携带任意的头信息
+        config.addAllowedHeader("*");
+        //允许携带cookie，如果允许携带cookie，origin不能写成*
+        config.setAllowCredentials(true);
+        //cors注册类,拦截所有请求，进行cors验证
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(configurationSource);
+    }
+
+}
